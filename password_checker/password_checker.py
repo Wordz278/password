207d987df8d23669fa044abcb8c2a8115a85204f
Adrian Floyd <adrianshikwambana@umuzi.org>
2020-07-11 19:52:03 +0200

    modified the functions and added loggings

--- password_checker/password_checker.py
+++ password_checker/password_checker.py
@@ -1,77 +1,119 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# formatter = logging.Formatter('%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('password_checker/error.log')
+file_handler.setLevel(logging.ERROR)
+# file_handler.setFormatter(formatter)
+
+
+stream_handler = logging.StreamHandler()
+# stream_handler.setFormatter(formatter)
+
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
 class Password:
 
     def __init__(self, password):
         self.password = password
-        self.validate = True
 
     def password_exist(self):
         if not self.password:
-            # self.validate = False
             raise NameError("Password should exist")
-
-        if self.validate:
-            return self.validate
+        else:
+            return True
 
     def password_length(self):
         if len(self.password) < 8:
-            raise ValueError("Password should be longer than 8 characters")
-
-        if self.validate:
-            return self.validate
+            raise ValueError(
+                "Password should be longer or equal than 8 characters")
+        else:
+            return True
 
     def password_lowercase(self):
         if not any(char.islower() for char in self.password):
             raise ValueError(
                 "Password should have atleast one lowercase letter")
-
-        if self.validate:
-            return self.validate
+        else:
+            return True
 
     def password_uppercase(self):
         if not any(char.isupper() for char in self.password):
             raise ValueError(
                 "Password should have at least one uppercase letter")
-
-        if self.validate:
-            return self.validate
+        else:
+            return True
 
     def password_digit(self):
         if not any(char.isdigit() for char in self.password):
             raise ValueError(
                 "Password should at least have one digit")
-
-        if self.validate:
-            return self.validate
+        else:
+            return True
 
     def password_special_character(self):
         special_character = ['%', '&', '*', '"', "'"]
-        self.character = special_character
 
-        if not any(char in self.character for char in self.password):
+        if not any(char in special_character for char in self.password):
             raise ValueError(
-                f"Password should have at least one of the symbols {special_character}")
+                f"Password should have at least one of the special characters {special_character}")
+        else:
+            return True
 
-        if self.validate:
-            return self.validate
+    def password_is_ok(self):
+        pass_gauge = 0
 
-    # def password_is_valid(self, password):
-    #     pass
+        if self.password_exist() == True:
+            pass_gauge += 1
+        else:
+            return False
 
-    # def password_is_ok(self, pass_is_valid):
-    #     pass
+        if self.password_length() == True:
+            pass_gauge += 1
+        else:
+            return False
+
+        if self.password_digit() == True:
+            pass_gauge += 1
+        else:
+            return False
+
+        if self.password_lowercase() == True:
+            pass_gauge += 1
+        else:
+            return False
+
+        if self.password_uppercase() == True:
+            pass_gauge += 1
+        else:
+            return False
+
+        if self.password_special_character() == True:
+            pass_gauge += 1
+        else:
+            return False
+
+        if pass_gauge > 3:
+            return True
+        else:
+            logger.debug("User password is not ok")
 
 
 if __name__ == '__main__':
 
-    password = "aAbBcCdDeEfF2*"
-    password_valid = Password(password)
-
-    special_character = ['%', '&', '*', '"', "'"]
+    # password = "a2Maddsa%"
+    password = "a2Madds%"
 
     try:
-        if (password_valid.password_special_character()):
-            print("Password is valid")
+        if (Password(password).password_is_ok() == True):
+            logger.debug("User password is ok")
         else:
-            print("Invalid Password !!")
+            logger.debug("User password is not ok")
     except (NameError, ValueError) as error:
-        print(error)
+        logger.error(error)
