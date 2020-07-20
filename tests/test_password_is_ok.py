8b2fdbbf073f9d043c5fcc95fad016afa053e606
Adrian Floyd <adrianshikwambana@umuzi.org>
2020-07-21 01:22:21 +0200

    modified the test file

--- tests/test_password_is_ok.py
+++ tests/test_password_is_ok.py
@@ -1,7 +1,4 @@
 from password_checker.password_checker import Password
 
-# Testing password is ok
-password_test = "De9fdtvdo%"
-
 def test_password_is_ok():
-    assert Password(password_test).password_is_ok() == True
+    assert Password("De9fdtvdo%").password_is_ok() == True
